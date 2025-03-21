9563b34a4338203efcb5739281a437aab7af6a5a
Suraj <Suraj@digi9.co.in>
2025-03-21 18:29:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import VendorList from "./module/admin/Vendorlist/Vendorlist";
 import Login from "./pages/Login/Login";
 import AdminPayment from "./module/admin/Payment/AdminPayment";
 import VendorPayment from "./module/admin/VendorPayment/VendorPayment";
+import RegistrationSuccessfully from "./module/vendor/RegistrationSuccessfully/RegistrationSuccessfully";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/registration-successfully" element={<RegistrationSuccessfully />} />  
         {/* Layout wraps all child routes */}
         <Route path="/" element={<Layout />}>
           {/* Admin Dashboard Route */}
