3382fd5506b004f2579607f234ad6e2ba62c873a
Roshnipatil31 <Roshni@digi9.co.in>
2025-03-22 10:22:05 +0530

    login integration

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,12 +2,19 @@ import axiosConfig from "../config/axiosConfig";
 
 export const createUser = async (userData) => {
   try {
-    console.log("Sending user data:", userData); // Debugging log
     const response = await axiosConfig.post("/user/create", userData);
-    console.log("User created successfully:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error creating user:", error.response ? error.response.data : error.message);
     throw error.response ? error.response.data : new Error("Server error");
   }
 };
+
+export const loginUser = async (user_id) => {
+    try {
+      const response = await axiosConfig.post("/user/get-user", { user_id });
+      return response.data;
+    } catch (error) {
+      throw error.response ? error.response.data : new Error("Server error");
+    }
+  };
+  
